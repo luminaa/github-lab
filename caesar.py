66230adb9d9ece4841016e6bb028facad5db922c
luminaa <rairahual7@gmail.com>
2023-04-11 15:55:07 -0400

    added caesar.py

---
+++ caesar.py
@@ -0,0 +1,22 @@
+import sys
+
+
+def caesar_encrpyt(text, shift):
+    result = ""
+
+    for i in range(len(text)):
+        char = text[i]
+        if (char.isupper()):
+            result += chr((ord(char) + shift-65) % 26 + 65)
+        else:
+            result += chr((ord(char) + shift - 97) % 26 + 97)
+
+    return result
+
+
+if __name__ == '__main__':
+    text = sys.argv[1]
+    shift = int(sys.argv[2])
+    print("Text  : " + text)
+    print("Shift : " + str(shift))
+    print("Cipher: " + caesar_encrpyt(text, shift))
