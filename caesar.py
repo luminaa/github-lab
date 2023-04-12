7ebe489b1bb2daf3cb5b1d07939c6117eb81865d
luminaa <rairahual7@gmail.com>
2023-04-11 20:07:52 -0400

    made each line of output a maximum of 10 blocks

--- caesar.py
+++ caesar.py
@@ -1,7 +1,7 @@
 import sys
 
 
-def caesar_encrpyt(text, shift):
+def caesar_encrypt(text, shift):
     result = ""
     text = text.upper()
 
@@ -16,12 +16,12 @@ def caesar_encrpyt(text, shift):
         #     result += char
         #     continue
 
-        # shifts the characters by specified number of places
-        result += chr(((ord(char) + shift-65) % 26) + 65)
+        # shift the characters by the specified number of places
+        result += chr(((ord(char) + shift - 65) % 26) + 65)
 
     # split the result into blocks of 5 characters and join them with spaces
-    blocks = [result[i:i+5] for i in range(0, len(result), 5)]
-    blocks = [' '.join(blocks[i:i+10]) for i in range(0, len(blocks), 5)]
+    blocks = [result[i:i + 5] + " " for i in range(0, len(result), 5)]
+    blocks = [''.join(blocks[i:i + 10]) for i in range(0, len(blocks), 10)]
     result = '\n'.join(blocks)
 
     return result
@@ -32,4 +32,4 @@ if __name__ == '__main__':
     shift = int(sys.argv[2])
     print(f"Text  : {text}")
     print(f"Shift : {shift}")
-    print(f"Cipher: {caesar_encrpyt(text, shift)}")
+    print(f"Cipher: \n{caesar_encrypt(text, shift)}")
