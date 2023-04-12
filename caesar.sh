5cb8d7959899cf0c7afc62480e6b586f96c1c44a
luminaa <rairahual7@gmail.com>
2023-04-11 20:49:13 -0400

    corrected file path

--- caesar.sh
+++ caesar.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python caesar_cipher.py "Congress shall make no law respecting an establishment of religion, or prohibiting the free exercise thereof; or abridging the freedom of speech, or of the press; or the right of the people peaceably to assemble, and to petition the government for a redress of grievances" 1
+python caesar.py "Congress shall make no law respecting an establishment of religion, or prohibiting the free exercise thereof; or abridging the freedom of speech, or of the press; or the right of the people peaceably to assemble, and to petition the government for a redress of grievances" 1
